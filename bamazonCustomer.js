6677d745175a8bf6186de7c81f7c0540f48bd66f
Morgan M <morgan.maxwell20@gmail.com>
2019-01-31 10:35:25 -0700

    clean code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,26 +21,13 @@ var questions = [{
     type: "input",
     message: "How many would you like to buy?"
 }];
-// open connection to database
+var confirmation = [{
+    name: "confirm",
+    type: "confirm",
+    message: "Would you like to make another purchase?"
+}];
+var seperator = "\n----------------------------------------------------------\n";
 connection.connect();
-function interaction(res) {
-    // ask the questions, and then check quantities of product selected
-    inquirer.prompt(questions)
-        .then(function (ans) {
-            if (res[ans.ProductPick - 1].stock_quantity - ans.Quantity < 0) {
-                console.log("\nInsufficient quantities! Please pick another product.\n");
-                interaction(res);
-            }
-            else {
-                var newQuantity = res[ans.ProductPick - 1].stock_quantity - ans.Quantity;
-                connection.query(
-                    "UPDATE products SET stock_quantity = ? WHERE id = ?", 
-                    [newQuantity, ans.ProductPick]
-                )
-                connection.end();
-            }
-        });
-};
 // Display database products and ask what one customer wants
 function customerUI() {
     connection.query(
@@ -49,15 +36,45 @@ function customerUI() {
                 throw err;
             };
             for (var i = 0; i < res.length; i++) {
-                console.log("\nItem Id: " + res[i].id +
-                    "\nItem Name: " + res[i].product_name +
-                    "\nDepartment: " + res[i].department_name +
-                    "\nPrice: " + res[i].price +
-                    "\nQuantity On Hand: " + res[i].stock_quantity + "\n\n");
+                console.log("\nItem Id: " + res[i].id + seperator + 
+                    "Item Name: " + res[i].product_name + seperator +
+                    "Department: " + res[i].department_name + seperator + 
+                    "Price: " + res[i].price + seperator +
+                    "Quantity On Hand: " + res[i].stock_quantity + seperator + "\n");
             };
             interaction(res);
         }
     );
 };
-
+function interaction(res) {
+    // ask the questions, and then check quantities of product selected
+    inquirer.prompt(questions)
+        .then(function (ans) {
+            // if there isn't enough stock, display an error, then 
+            // loop the function with recursion
+            if (res[ans.ProductPick - 1].stock_quantity - ans.Quantity < 0) {
+                console.log(seperator + "Insufficient quantities! Please pick another product." + seperator);
+                interaction(res);
+            }
+            else {
+                // else, update the quantity in the database
+                var newQuantity = res[ans.ProductPick - 1].stock_quantity - ans.Quantity;
+                console.log(seperator + "Purchase Successful!" + seperator)
+                connection.query(
+                    "UPDATE products SET stock_quantity = ? WHERE id = ?",
+                    [newQuantity, ans.ProductPick]
+                );
+                // this is asking if the customer would like to make another purchase
+                inquirer.prompt(confirmation)
+                    .then(function (ans) {
+                        if (ans.confirm) {
+                            interaction(res);
+                        }
+                        else {
+                            connection.end();
+                        };
+                    });
+            };
+        });
+};
 customerUI();
