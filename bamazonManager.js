8b7c2b6ac040f7a7c0365b25603537086e3e63df
Morgan M <morgan.maxwell20@gmail.com>
2019-02-04 15:22:51 -0700

    Took out recursion to get to MVP, I'll research why it would crash

--- bamazonManager.js
+++ bamazonManager.js
@@ -18,14 +18,41 @@ var questions = [{
         ["View Products for Sale",
             "View Low Inventory",
             "Add to Inventory",
-            "Add New Product",
-            "Log Off"],
+            "Add New Product"
+        ],
     message: "Pick an Action to take\n"
 }];
-var confirmation = [{
-    name: "confirm",
-    type: "confirm",
-    message: "Are you sure you want to Log Off?"
+// add inventory
+var addInventory = [{
+    name: "addInventory",
+    type: "input",
+    message: "Input the ID of the product: "
+},
+{
+    name: "stock",
+    type: "input",
+    message: "Input the stock quantity: "
+}];
+// add new product
+var addNew = [{
+    name: "productName",
+    type: "input",
+    message: "Input the product_name to add: ",
+},
+{
+    name: "departmentName",
+    type: "input",
+    message: "Input the department_name: ",
+},
+{
+    name: "pricing",
+    type: "input",
+    message: "Input the price per unit(numbers only): ",
+},
+{
+    name: "stockQuantity",
+    type: "input",
+    message: "Input the stock on hand(numbers only): "
 }];
 var seperator = "\n----------------------------------------------------------\n";
 connection.connect();
@@ -33,25 +60,26 @@ connection.connect();
 function managerUI() {
     inquirer.prompt(questions)
         .then(function (ans) {
-            if (ans.managerOptions[0]) {
-                viewProducts();
-            }
-            else if (ans.managerOptions[1]) {
-                viewLowInv();
-            }
-            else if (ans.managerOptions[2]) {
-                addInv();
-            }
-            else if (ans.managerOptions[3]) {
-                addNewProduct();
-            }
-            else if (ans.managerOptions[4]) {
-                logOff();
-            }
-            else {
-                console.log("You broke it, go back");
-                managerUI();
-            }
+            switch (ans.managerOptions) {
+                case "View Products for Sale":
+                    viewProducts();
+                    break;
+                case "View Low Inventory":
+
+                    viewLowInv();
+                    break;
+                case "Add to Inventory":
+                    addInv();
+                    break;
+                case "Add New Product":
+                    addNewProduct();
+                    break;
+                default:
+                    console.log("You broke it, go back");
+                    managerUI();
+                    break;
+
+            };
         });
 };
 // Display database products
@@ -68,10 +96,9 @@ function viewProducts() {
                     "Price: " + res[i].price + seperator +
                     "Quantity On Hand: " + res[i].stock_quantity + seperator + "\n");
             };
-            interaction(res);
         }
     );
-    managerUI();
+    endConnection();
 };
 // Display products with stock_quantity < 5
 function viewLowInv() {
@@ -80,38 +107,54 @@ function viewLowInv() {
             if (err) {
                 throw err;
             };
-            for (var i = 0; i < res.length; i++) {
-                console.log("\nItem Id: " + res[i].id + seperator + 
-                    "Item Name: " + res[i].product_name + seperator +
-                    "Department: " + res[i].department_name + seperator + 
-                    "Price: " + res[i].price + seperator +
-                    "Quantity On Hand: " + res[i].stock_quantity + seperator + "\n");
+            if (res === undefined || res.length < 1) {
+                console.log("No Low Inventory Products");
+            }
+            else {
+                for (var i = 0; i < res.length; i++) {
+                    console.log("\nItem Id: " + res[i].id + seperator +
+                        "Item Name: " + res[i].product_name + seperator +
+                        "Department: " + res[i].department_name + seperator +
+                        "Price: " + res[i].price + seperator +
+                        "Quantity On Hand: " + res[i].stock_quantity + seperator + "\n");
+                };
             };
-            interaction(res);
         }
     );
-    managerUI();
+    endConnection();
 };
 // Increase stock_quantity of a product
 function addInv() {
+    inquirer.prompt(addInventory)
+        .then(function (ans) {
+            var id = ans.addInventory;
+            var stock = ans.stock;
+            connection.query(
+                'UPDATE products SET stock_quantity = stock_quantity + ? WHERE id = ?',
+                [stock, id]
+            );
+            endConnection();
 
-    managerUI();
+        });
 };
 // Insert a new table row for a product
 function addNewProduct() {
-
-    managerUI();
-};
-// confirm manager wants to close UI
-function logOff() {
-    inquirer.prompt(confirmation)
+    inquirer.prompt(addNew)
         .then(function (ans) {
-            if (ans.confirm) {
-                connection.end();
-            }
-            else {
-                managerUI();
-            };
+            var product = ans.productName;
+            var department = ans.departmentName;
+            var price = ans.pricing;
+            var stock = ans.stockQuantity;
+            connection.query(
+                'INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES (?, ?, ?, ?)',
+                [product, department, price, stock]
+            );
+            endConnection();
         });
 };
+// end connection
+function endConnection() {
+    connection.end();
+}
+// start the whole thing off
 managerUI();
